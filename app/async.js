d11ccf9ae6278f3cd8afd619b6a8f94fbefc2644
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-03 14:30:53 +0100

    async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise((resolve, reject) => {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
-
+    function sort(a, b) {
+      return a.name > b.name;
+    }
+    return new Promise((resolve, reject) => {
+      $.ajax(url).done(data => {
+        resolve(data.people.sort(sort).map(object => {
+          return object.name;
+        }));
+      }).fail(err => {
+        reject(err);
+      })
+    });
   }
 };
