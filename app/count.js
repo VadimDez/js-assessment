3e4886f2bb74d47bdeb0c1f271347c71807ba58f
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-03 15:28:54 +0100

    counter

--- app/count.js
+++ app/count.js
@@ -2,6 +2,30 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var Counter = function (start, end) {
+      this.start = start;
+      this.end = end;
+      this.i = start;
+      this.count();
+    };
 
+    Counter.prototype.count = function () {
+      var that = this;
+
+      console.log(that.i++);
+      this.interval = setInterval(function () {
+        if (that.i >= that.end) {
+          clearInterval(that.interval);
+        }
+
+        console.log(that.i++);
+      }, 100);
+    };
+
+    Counter.prototype.cancel = function () {
+      clearInterval(this.interval);
+    };
+
+    return new Counter(start, end);
   }
 };
